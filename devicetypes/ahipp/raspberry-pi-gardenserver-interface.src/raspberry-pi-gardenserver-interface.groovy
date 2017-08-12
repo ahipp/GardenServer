40144895b4d70efd7a3a3f497c114a18717df9bc
Adam Hipp <hipp.adam@gmail.com>
2017-08-12 15:58:20 -0500

    GET and POST requests to pi working and updating UI. Code badly needs refactoring and specifics need to be worked out.

--- devicetypes/ahipp/raspberry-pi-gardenserver-interface.src/raspberry-pi-gardenserver-interface.groovy
+++ devicetypes/ahipp/raspberry-pi-gardenserver-interface.src/raspberry-pi-gardenserver-interface.groovy
@@ -13,12 +13,21 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+ 
+preferences {
+        input("ip", "string", title:"IP Address", description: "192.168.1.150", defaultValue: "192.168.1.150", required: true, displayDuringSetup: true)
+        input("port", "string", title:"Port", description: "8000", defaultValue: 8000, required: true, displayDuringSetup: true)
+}
+
 metadata {
 	definition (name: "Raspberry Pi Gardenserver Interface", namespace: "ahipp", author: "Adam Hipp") {
-    	attribute "internalSwitch", "string"
         attribute "soilTemp", "string"
-        command "turnOn"
-        command "turnOff"
+        attribute "coilSwitch", "string"
+        attribute "fishTankSwitch", "string"
+        command "turnOnCoil"
+        command "turnOffCoil"
+        command "turnOnFishTank"
+        command "turnOffFishTank"
 	}
 
 	simulator {
@@ -26,74 +35,150 @@ metadata {
 	}
 
 	tiles(scale: 2) {        
-        standardTile("automatedControlTile", "device.internalSwitch", width: 2, height: 2) {
-            state "off", label:'Off', icon:"st.switches.switch.off", backgroundColor:"#ffffff"
-            state "on", label:'On', icon:"st.switches.switch.on", backgroundColor:"#390172"
-        } 
-        
-        standardTile("turnOnTile", "device.turnOn", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-        	state "default", action:"turnOn", label: "Turn On", displayName: "Turn On"
+        standardTile("coilSwitch", "device.coilSwitch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+        	state "off", action:"turnOnCoil", label: "Turn On Coil", displayName: "Turn On Coil", icon:"st.switches.switch.off", backgroundColor: "#ffffff"
+            state "on", action:"turnOffCoil", label: "Turn Off Coil", displayName: "Turn Off Coil", icon:"st.switches.switch.on", backgroundColor: "#009900"
 		}
         
-        standardTile("turnOffTile", "device.turnOff", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-        	state "default", action:"turnOff", label: "Turn Off", displayName: "Turn Off"
+        standardTile("fishTankSwitch", "device.fishTankSwitch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+        	state "off", action:"turnOnFishTank", label: "Turn On Fish Tank", displayName: "Turn On Fish Tank", icon:"st.switches.switch.off", backgroundColor: "#ffffff"
+            state "on", action:"turnOffFishTank", label: "Turn Off Fish Tank", displayName: "Turn Off Fish Tank", icon:"st.switches.switch.on", backgroundColor: "#009900"
 		}
         
         valueTile("soilTemp", "device.soilTemp", decoration: "flat", width: 2, height: 2) {
         	state "soilTemp", label:'${currentValue}', unit: "F"
         }
         
-        main("automatedControlTile")
-        details(["automatedControlTile","turnOnTile","turnOffTile","soilTemp"])
+        main("soilTemp")
+        details(["coilSwitch","fishTankSwitch","soilTemp"])
     }       
-
 }
 
 def installed() {
 	log.debug "installed"
 }
 
-// parse events into attributes
+def updated() {
+	log.debug "updated"
+    getSoilTemp()
+}
+
+def initialize() {
+	log.debug "initialize"
+    runEvery1Minute(getSoilTemp)
+}
+
 def parse(String description) {
     log.debug "parse description: $description"
+}
 
-    def attrName = null
-    def attrValue = null
+def getSoilTemp() {
+	log.debug "getSoilTemp"
+    def uri = "/getTemperature"
+    def body = [sensorName: "tempSensor1"]
+	getAction(uri, body)
+}
 
-    if (description?.startsWith("on/off:")) {
-        log.debug "switch command"
-        attrName = "switch"
-        attrValue = description?.endsWith("1") ? "on" : "off"
+def turnOffCoil() {
+	log.debug "turnOffCoil()"
+    def uri = "/issueCommand"
+    def body = [command: "soilCoilHeaterOff"]
+    postAction(uri, body)
+}
+
+def turnOnCoil() {
+	log.debug "turnOnCoil()"
+    def uri = "/issueCommand"
+    def body = [command: "soilCoilHeaterOn"]
+    postAction(uri, body) 
+}
+
+def turnOffFishTank() {
+	log.debug "turnOffFishTank()"
+    def uri = "/issueCommand"
+    def body = [command: "fishTankHeaterOff"]
+    postAction(uri, body)
+}
+
+def turnOnFishTank() {
+	log.debug "turnOnFishTank()"
+    def uri = "/issueCommand"
+    def body = [command: "fishTankHeaterOn"]
+    postAction(uri, body) 
+}
+
+private getAction(uri, body) {
+  setDeviceNetworkId(ip,port)  
+  
+  def hubAction = new physicalgraph.device.HubAction(
+    [
+    method: "GET",
+    path: uri,
+    body: body
+    ],
+    device.deviceNetworkId,
+    [callback: "getCallbackHandler"]
+  )
+  
+  log.debug("Executing hubAction on " + getHostAddress())
+  hubAction 
+}
+
+private getCallbackHandler(physicalgraph.device.HubResponse hubResponse) {
+	log.debug "$hubResponse.body"
+    sendEvent(name: "soilTemp", value: hubResponse.body.take(4))
+}
+
+private postAction(uri, body){
+  setDeviceNetworkId(ip,port)  
+  
+  def hubAction = new physicalgraph.device.HubAction(
+    [
+    method: "POST",
+    path: uri,
+    body: body
+    ],
+    device.deviceNetworkId,
+    [callback: "postCallbackHandler"]
+  )
+  
+  log.debug("Executing hubAction on " + getHostAddress())
+  hubAction   
+}
+
+private postCallbackHandler(physicalgraph.device.HubResponse hubResponse) {
+    if (hubResponse.body.equalsIgnoreCase(/"soilCoilHeaterOff issued"/)) {
+    	log.debug "coilSwitch off event"
+    	sendEvent(name: "coilSwitch", value: "off")
+    } else if (hubResponse.body == /"soilCoilHeaterOn issued"/) {
+    	log.debug "coilSwitch on event" 
+    	sendEvent(name: "coilSwitch", value: "on")
+    } else if (hubResponse.body.equalsIgnoreCase(/"fishTankHeaterOff issued"/)) {
+    	log.debug "fishTankSwitch off event"
+    	sendEvent(name: "fishTankSwitch", value: "off")
+    } else if (hubResponse.body == /"fishTankHeaterOn issued"/) {
+    	log.debug "fishTankSwitch on event" 
+    	sendEvent(name: "fishTankSwitch", value: "on")
     }
-
-    def result = createEvent(name: attrName, value: attrValue)
-
-    log.debug "Parse returned ${result?.descriptionText}"
-    return result
 }
 
-def getSoilTemp(data) {
-	log.debug "getSoilTemp $data"
-	sendEvent(name: "soilTemp", value: "$data")
+private setDeviceNetworkId(ip,port){
+  	def iphex = convertIPtoHex(ip)
+  	def porthex = convertPortToHex(port)
+  	device.deviceNetworkId = "$iphex:$porthex"
+  	log.debug "Device Network Id set to ${iphex}:${porthex}"
 }
 
-def turnOff() {
-	log.debug "turnOff()"
-    getSoilTemp("75")
-	sendEvent(name: "internalSwitch", value: "off")	
+private getHostAddress() {
+	return "${ip}:${port}"
 }
 
-def turnOn() {
-	log.debug "turnOn()"
-    getSoilTemp("70")
-	sendEvent(name: "internalSwitch", value: "on")	
+private String convertIPtoHex(ipAddress) { 
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    return hex
 }
 
-def on() {
-	log.debug "on()"
+private String convertPortToHex(port) {
+	String hexport = port.toString().format( '%04x', port.toInteger() )
+    return hexport
 }
-
-def off() {
-	log.debug "off()"
-}
-
